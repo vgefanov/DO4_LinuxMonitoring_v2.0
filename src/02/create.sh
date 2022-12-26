f5123efb7977d194a825adfa76c8eb8dd260e8fb
Alvin Thalia <alvintha@in-f2.nsk.21-school.ru>
2022-12-26 19:06:07 +0700

    26.12.2022part02

---
+++ src/02/create.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+function path_generator {
+  local number_of_folders=`find ~ -perm -u+w -type d | wc -l`
+  local number=$((1 + $RANDOM % $number_of_folders))
+  local dir_to_embed=`find ~ -perm -u+w -type d | head -$number | tail +$number`
+  echo $dir_to_embed
+}
+
+function name_generator {
+  local length=`expr length $1`
+  for (( i=1; i<=$length; i++ ))
+  do
+    local arr[$i]=`expr substr $1 $i 1`
+    local name+="${arr[$i]}"
+  done
+  echo $name
+}
+
+function add_sign {
+  local name=$1
+  local symbol=$3
+  local length=`expr length $2`
+  for (( i=1; i<=$length; i++ ))
+  do
+    local arr[$i]=`expr substr $2 $i 1`
+  done
+  local index=`expr index $name ${arr[$symbol]}`
+  local tmp_name=$(expr substr $name 1 $index)${arr[$symbol]}$(echo ${name:$index})
+  local name=$tmp_name
+  echo $name
+}
+
+free_space=$(df / |  head -2 | tail +2 | awk '{printf("%d", $4)}')
+touch logs_file.txt
+echo -n "" > logs_file.txt
+echo "Script start $start_time" | tee -a ~/LM02/02/logs_file.txt
+while [[ $free_space -ge $((1000000 + $(($file_size * 1000)))) ]]
+do
+  path_to_embed=$(path_generator)
+  count_dir=$((1 + $RANDOM % 100))
+  count_files=$((1 + $RANDOM % 100))
+  for (( j=0; j<$count_dir; j++))
+  do
+    cd $path_to_embed
+
+    # create folder
+    name_dir=$(name_generator $letters_folder_names)
+    date=$(date +%D | awk -F / '{print $2$1$3}')
+    tmp_name_dir=$name_dir"_"$date
+    char=0
+    while [ -e "$tmp_name_dir" ] || [[ `expr length $name_dir` -lt 4 ]];
+    do
+      if [[ char -ge `expr length $letters_folder_names` ]] || [[ `expr length $letters_folder_names` -eq 1 ]]
+        then
+          char=1
+        else
+          char=$(($char+1))
+      fi
+      name_dir=$(add_sign $name_dir $letters_folder_names $char)
+      tmp_name_dir=$name_dir"_$date"
+    done
+    sudo mkdir $tmp_name_dir
+    echo $(pwd)"/$tmp_name_dir/" `date +%F" "%H":"%M` >> ~/LM02/02/logs_file.txt
+
+    # create files
+    for ((f=0; f < $count_files; f++))
+    do
+      char_file=0
+      name_file=$(name_generator $file_name)
+      file=$tmp_name_dir"/$name_file"_"$date.$file_extension"
+      while [ -e $file ] || [[ `expr length $name_file` -lt 4 ]];
+      do
+        if [[ char_file -ge `expr length $letters_folder_names` ]] || [[ `expr length $file_name` -eq 1 ]]
+          then
+            char_file=1
+          else
+            char_file=$(($char_file+1))
+        fi
+        name_file=$(add_sign $name_file $file_name $char_file)
+        file=$tmp_name_dir"/$name_file"_"$date.$file_extension"
+      done
+      sudo fallocate -l $file_size"MB" $file
+      echo $(pwd)"/$file" `date +%F" "%H":"%M` `ls -lh $file | awk '{print $5}'` >> ~/LM02/02/logs_file.txt
+      free_space=$(df / |  head -2 | tail +2 | awk '{printf("%d", $4)}')
+      if [[ $free_space -le $((1000000 + $(($file_size * 1000)))) ]]; then
+        exit 1
+      fi
+    done
+  done
+done
---
+++ src/02/main.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+start_script=`date +%s`
+length_folder_name=$[ $(echo $1 | wc -m) - 1 ]
+length_file_name=$[ $(echo $(echo $2 | awk -F. '{print $1}') | wc -m) - 1 ]
+length_file_extension=$[ $(echo $(echo $2 | awk -F. '{print $2}') | wc -m) - 1 ]
+declare -x start_time=`date +%F" "%H:%M:%S`
+declare -x letters_folder_names=$1
+declare -x file_name=$(echo $2 | awk -F. '{print $1}')
+declare -x file_extension=$(echo $2 | awk -F. '{print $2}')
+declare -x file_size=$[ $(echo $3 | tr -d [:alpha:]) ]
+
+if [ $# -ne 3 ]; then
+    echo "Error. Need to enter 3 parameter, you have entered $# parameters!"
+elif [[ $length_folder_name -gt 7 ]]; then
+    echo "Parameter 1. Must not exceed 7 characters. Change parameter 1"
+elif [[ $letters_folder_names =~ [^A-Za-z] ]]; then
+    echo "Parameter 1. A list of English alphabet letters. Change parameter 1"
+elif [[ $length_file_name -gt 7 ]] || [[ $length_file_extension -gt 3 ]] || [[ $length_file_name -lt 1 ]] || [[ $length_file_extension -lt 1 ]]; then
+    echo "Parameter 2. The name should not exceed 7 characters, the extension should not exceed 3 characters. Example abc.abc. Change parameter 2"
+elif [[ $2 =~ [^A-Za-z.] ]]; then
+    echo "Parameter 2. A list of English alphabet letters. Change parameter 2"
+elif [[ !($3 =~ [Mm]b$) ]]; then
+    echo "Parameter 3. File size in megabytes, specify mb or Mb. Change parameter 3"
+elif [[ $file_size -gt 100 ]] || [[ $file_size -le 0 ]]; then
+    echo "Parameter 3. File size from 1 to 100 megabytes. Change parameter 3"
+else
+    ./create.sh
+    end_script=`date +%s`
+    end_time=`date +%F" "%H:%M:%S`
+    echo "Script finish $end_time" | tee -a ~/LM02/02/logs_file.txt
+    result_time=$(($end_script-$start_script))
+    if [[ $result_time -lt 60 ]]; then
+        minuts=0
+        seconds=$result_time
+    else
+        minuts=$(($result_time/60))
+        seconds=$(($result_time%60))
+    fi
+    echo "Total running time of the script $minuts minut $seconds sec" | tee -a ~/LM02/02/logs_file.txt
+fi
+
+# bash main.sh azsx abcd.efg 100mb
+# sudo find ~ -type f -name "*.efg" -delete
+# df -h /
