c3a80339913f9bb6c6f60471028af83350a0fa24
Alvin Thalia <alvintha@in-f2.nsk.21-school.ru>
2023-01-25 13:45:18 +0700

    25.01.2023part 07-09

--- src/09/main.sh
+++ src/09/main.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sudo apt install nginx-light
+# sudo apt install nginx
 sudo cp ./nginx/nginx.conf /etc/nginx/nginx.conf
 sudo nginx -s reload
-sudo cp prometheus.yml /etc/prometheus/prometheus.yml
-systemctl restart prometheus.service
 chmod +x my_nodes.sh
 while sleep 3
 do
